04d12ff65346cfc279fad315a0bef663efbd4e60
xela <xelaxela13@gmail.com>
2023-05-08 19:55:05 +0200

    messages

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,10 @@
 from django.conf import settings
+from django.contrib import messages
 from django.contrib.auth import login
+from django.contrib.auth import login as auth_login
 from django.contrib.auth.views import LoginView as AuthLoginView
+from django.http import HttpResponseRedirect
+from django.utils.translation import gettext_lazy as _
 from django.views.generic import FormView
 
 from accounts.model_forms import RegistrationForm, AuthenticationForm
@@ -19,3 +23,9 @@ class RegistrationView(FormView):
 
 class LoginView(AuthLoginView):
     form_class = AuthenticationForm
+
+    def form_valid(self, form):
+        """Security check complete. Log the user in."""
+        auth_login(self.request, form.get_user())
+        messages.success(self.request, _('Welcome back!'))
+        return HttpResponseRedirect(self.get_success_url())
